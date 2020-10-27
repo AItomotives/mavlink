7f11e77b8aa96f26da0bf84d9c4e569d21ebe4af
cms2146 <cms2146@rit.edu>
2020-10-27 17:41:49 -0400

    Initial rewards commit

---
+++ cmake-build-debug/CMakeFiles/ASLUAV.xml-v1.0.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "ASLUAV.xml-v1.0-stamp"
+  "CMakeFiles/ASLUAV.xml-v1.0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ASLUAV.xml-v1.0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/test.xml-v2.0.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.xml-v2.0"
+  "test.xml-v2.0-stamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/test.xml-v2.0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
